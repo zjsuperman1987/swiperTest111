4e95252633e95e08a17ccc6e4d05144455803121
zjsuperman1987 <250529492@qq.com>
2018-11-20 15:32:25 +0800

    another branch

--- reWrCommdity.js
+++ reWrCommdity.js
@@ -3,6 +3,11 @@ window.onload = function() {
     commdityDetail.initScroll();
 }
 
+ 
+
+
+
+ 
 var commdityDetail = (function() {
     var my = {};
 
@@ -10,10 +15,9 @@ var commdityDetail = (function() {
         initSwiper: function() {
             my.scrollTransform = [];
             my.scrollers = [];
-            my.scrollersDom = $('.swiper-slide').children();
+            my.scrollersDom = $('.swiper-tab .swiper-slide:not(.not-collection)').children();
             my.firstPosition = -($('.h_searchWrapper').outerHeight() + $('.iw_info').outerHeight());
-            my.secondPosition = -555;
-
+            my.secondPosition = -553;
 
             // 创建变量保存scroller对象
             $.each(my.scrollersDom, function(i, item) {
@@ -55,14 +59,22 @@ var commdityDetail = (function() {
             var index = index ? index : 0,
                 contentTransform = contentTransform ? contentTransform : 0,
                 scrollTransform = my.scrollTransform[index],
-                scroller;
+                scroller,
+
+                title_index = 0,
+                old_goodHeight,
+                goodHeight = $('.goodsList_right li').eq(title_index).outerHeight() - titleHeight,
+                titleHeight = $('.rn_title').eq(0).outerHeight();
+
+
+
 
 
 
             function scrollAnimate() {
                 var y = this.y >> 0;
 
-                console.log(y, scrollAnimate.caller.arguments[0], this.options.fixed, my.scrollTransform[my.mySwiper.activeIndex],this);
+                console.log(y, scrollAnimate.caller.arguments[0], this.options.fixed, my.scrollTransform[my.mySwiper.activeIndex], this);
 
                 // 初始定位位置 -213 
                 if (y < my.firstPosition && this.startY > my.firstPosition || y > my.firstPosition && this.startY < my.firstPosition) {
@@ -70,7 +82,6 @@ var commdityDetail = (function() {
                     this.options.fixed = true; //进入后不执行动能
                     console.log('骚骚的进入', scrollAnimate.caller.arguments[0], this.startY, y);
                 }
-
 
                 if (y > my.firstPosition) {
                     $('.container').css('transform', 'translate(0,' + y + 'px)');
@@ -94,18 +105,59 @@ var commdityDetail = (function() {
                     $('.container').css('transform', 'translate(0,' + my.firstPosition + 'px)');
                     $(this.scroller).css('transform', 'translate(0,' + (y - my.firstPosition) + 'px)');
                 }
- 
+
 
                 // 二级定位
-                if (y < my.secondPosition) {
-                	$('.container').css('transform', 'translate(0,' + my.firstPosition + 'px)');
-                    $(this.scroller).css('transform', 'translate(0,' + (my.secondPosition - my.firstPosition) + 'px)');
-                    $('.rightNav').css('transform', 'translate(0,' + (y - my.secondPosition) + 'px)');
-                    
-                }else {
+                if (my.mySwiper.activeIndex === 0) {
 
 
+
+
+                    if (y < my.secondPosition) {
+                        $('.container').css('transform', 'translate(0,' + my.firstPosition + 'px)');
+                        $(this.scroller).css('transform', 'translate(0,' + (my.secondPosition - my.firstPosition) + 'px)');
+                        $('.goodsList_right').css('transform', 'translate(0,' + (y - my.secondPosition) + 'px)');
+
+                        if (this.directionY > 0) {
+                            if (!$('.rn_title').hasClass('active_rn_title_middle')) {
+                                $('.rn_title').eq(title_index).insertAfter('.goodsList_left').addClass('active_rn_title');
+                                $('.goodsList_right li').eq(title_index).addClass('goodsList_right_li_loseHead');
+                            }
+
+                            // 零界点
+                            if (y <= my.secondPosition - goodHeight) {
+                                $('.rn_title').eq(title_index).prependTo($('.goodsList_right li').eq(title_index)).addClass('active_rn_title_middle');
+                                title_index = title_index + 1;
+                                old_goodHeight = my.secondPosition - goodHeight;
+                                goodHeight += $('.goodsList_right li').eq(title_index).outerHeight() - titleHeight;
+                                
+                            }
+                            if (y < old_goodHeight - titleHeight) {
+                                alert(999)
+                                $('.rn_title').eq(title_index).insertAfter('.goodsList_left').addClass('active_rn_title');
+                                $('.goodsList_right li').eq(title_index).addClass('goodsList_right_li_loseHead');
+                            }
+                        } else {
+                            if (y > old_goodHeight - titleHeight) {
+                                $('.rn_title').eq(title_index).prependTo($('.goodList_right li').eq(title_index)).removeClass('active_rn_title');
+                                $('.goodsList_right li').eq(title_index).removeClass('goodsList_right_li_loseHead');
+                            }
+                            if (y > old_goodHeight) {
+                                $('.rin_title').eq(title_index - 1).insertAfter('.goodsList_left').removeClass('active_rn_title_middle');
+                                title_index = title_index - 1;
+                                old_goodHeight -= $('.goodsList_right li').eq(title_index).outerHeight();
+                            }
+                        }
+
+
+                    } else {
+                        $('.goodsList_right').css('transform', 'translate(0,0)');
+                        $('.rn_title').eq(title_index).prependTo($('.goodsList_right li').eq(title_index)).removeClass('active_rn_title');
+                        $('.goodsList_right li').eq(title_index).removeClass('goodsList_right_li_loseHead');
+                        title_index = 0;
+                    }
                 }
+
 
 
 
@@ -172,3 +224,20 @@ var commdityDetail = (function() {
 
     }
 }())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
